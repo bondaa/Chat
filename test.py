626974b781fd86ca205e172c051c95e7edbe5206
bondaraa <legand.bond@gmail.com>
2017-10-09 23:03:48 +0300

    Chat

---
+++ test.py
@@ -0,0 +1,13 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind(('0.0.0.0',11719))
+i=1
+while 1:
+    message = s.recv(128)
+    print (i,message)
+    i+=1
+
+    
